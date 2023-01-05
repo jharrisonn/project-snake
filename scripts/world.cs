1b5c22590adbc22c2cce90e4864afc05adf828cc
jharrisonn <101993161+jharrisonn@users.noreply.github.com>
2023-01-05 16:49:09 -0300

    Simple spawn test

--- scripts/world.cs
+++ scripts/world.cs
@@ -7,17 +7,18 @@ public class world : Node2D
 	// private int a = 2;
 	// private string b = "text";
 
+	PackedScene lTroop = GD.Load<PackedScene>("res://objects/troop.tscn");
+
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(float delta)
-	{
+	public override void _Process(float delta) {
 		if (Input.IsActionJustPressed("act")) {
-			GD.Print("Acting");
+			Node t = lTroop.Instance();
+			AddChild(t);
 		}
 	}
 }
